6c4e13f38811326ff016dd348cc20cf5e381e3bb
Jan Sodomka <h.sodomka@seznam.cz>
2026-04-19 19:05:58 +0200

    redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://www.primeclean.cz',
+  site: 'https://primeclean.cz',
   trailingSlash: 'never',
 
   build: {
